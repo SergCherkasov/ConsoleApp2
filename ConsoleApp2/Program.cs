b367f3dc4d3ef370efe9b5c91377ea4752f4f5f7
Admin <Admin@DESKTOP-34GRT87>
2023-03-01 17:15:29 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+for (int i = 0; i < 1000; i++)
+{
+    Console.WriteLine($"#:{i + 1}");
+    ThreadPool.QueueUserWorkItem(MyMethod);
+    Thread.Sleep(10000);
+}
+
+void MyMethod(object? obj)
+{
+    Thread thread = new Thread(() =>
+    {
+        thread = Thread.CurrentThread;
+    });
+
+    thread.IsBackground = true;
+    thread.Start();
+    Console.WriteLine($"ThreadID:{thread.ManagedThreadId}\nAmount of threds in the Pool:{ThreadPool.ThreadCount}\nThredIsBackground?:{thread.IsBackground}");
+}
